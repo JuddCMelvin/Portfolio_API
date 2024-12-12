21d71adbbedc39178d4ccdccebb3387242056f5a
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-12-12 10:34:40 -0500

    Remove .env file

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+//Create a Homepage route
+app.get('/', function(req,res){
+    //This gest sent to the client
+    res.send('hello world')
+})
+
+//Wildcard Route 
+app.get('*', function(req,res){
+    res.status(404).send('404 page')
+})
+
+//listen for connections
+app.listen(process.env.PORT,  function() {
+    console.log(`I am listening on port ${process.env.PORT}`)
+})
